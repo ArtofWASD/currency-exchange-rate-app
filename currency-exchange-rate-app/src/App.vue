00745daf42e386b81dfeeff49941b0c33e3a3121
Leonid <Chunl33666@yandex.ru>
2022-07-02 14:26:23 +0300

    feat: Vue3 + Vite

---
+++ currency-exchange-rate-app/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+</script>
+
+<template>
+  <h1 class="flex text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <h1>hahahah</h1>
+</template>
