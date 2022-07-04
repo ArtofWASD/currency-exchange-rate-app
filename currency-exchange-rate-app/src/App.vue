2f4aef24eae41f32f1fff9016b56ab8db1384f26
Leonid <Chunl33666@yandex.ru>
2022-07-05 02:55:00 +0300

    feat: axios try to fetch

--- currency-exchange-rate-app/src/App.vue
+++ currency-exchange-rate-app/src/App.vue
@@ -1,24 +1,34 @@
 <template lang="">
-  <main class="main flex flex-col items-center bg-gradient-to-r from-green-500 to-blue-500 min-h-screen">
-    <h1 class="text-3xl mt-40 text-white ">Курсы обмена валют</h1>
+  <main
+    class="main flex flex-col items-center bg-gradient-to-r from-green-500 to-blue-500 min-h-screen"
+  >
+    <h1 class="text-3xl mt-40 text-white">Курсы обмена валют</h1>
     <div class="flex gap-5 mt-10">
-   <inputField/>
+      <inputField />
     </div>
     <div>
-      {{result}}
+      {{ data }}
     </div>
   </main>
 </template>
 
 <script>
 import inputField from "./components/inputFiled.vue";
+import { ref } from "vue";
+import axios from "axios";
 export default {
   components: {
     inputField,
   },
-  setup(){
- 
-  }
+  setup() {
+    const data = ref(null);
+    axios
+      .get(
+        "https://currate.ru/api/?get=rates&pairs=USDRUB,EURRUB&key=a65e139fc72359d4597691114962a4de"
+      )
+      .then((data) => (result.data = data));
+    return { data };
+  },
 };
 </script>
 <style lang=""></style>
--- currency-exchange-rate-app/src/main.js
+++ currency-exchange-rate-app/src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import './index.css'
-const app = createApp(App)
-app.use(VueAxios, axios)
-app.mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import "./index.css";
+const app = createApp(App);
+app.use(VueAxios, axios);
+app.mount("#app");
